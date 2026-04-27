65fa6a8634285047180012061dd659d9f1ef5eb3
marni-xyz <xyellowmanx@gmx.de>
2026-04-27 14:57:02 +0200

    Added functions to toggle boost mode and timer

---
+++ custom_components/blauberg_s21/switch.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+from homeassistant.components.switch import SwitchEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from pybls21.client import S21Client
+from .const import DOMAIN
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    client: S21Client = hass.data[DOMAIN][config_entry.entry_id]
+    async_add_entities([
+        BlaubergS21BoostSwitch(client, config_entry),
+        BlaubergS21TimerSwitch(client, config_entry),
+    ])
+
+
+class BlaubergS21BoostSwitch(SwitchEntity):
+    _attr_icon = "mdi:fan-plus"
+    _attr_translation_key = "blauberg_s21_boost_switch"
+
+    def __init__(self, client: S21Client, config_entry: ConfigEntry) -> None:
+        self._client = client
+        self._config_entry = config_entry
+        self._attr_unique_id = f"blauberg_s21_{config_entry.unique_id}_boost_switch"
+
+    @property
+    def is_on(self) -> bool:
+        return self._client.device.is_boosting
+
+    async def async_turn_on(self, **kwargs) -> None:
+        await self._client.set_boost_on()
+
+    async def async_turn_off(self, **kwargs) -> None:
+        await self._client.set_boost_off()
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        return DeviceInfo(
+            identifiers={(DOMAIN, self._config_entry.unique_id)},
+        )
+
+
+class BlaubergS21TimerSwitch(SwitchEntity):
+    _attr_icon = "mdi:timer"
+    _attr_translation_key = "blauberg_s21_timer_switch"
+
+    def __init__(self, client: S21Client, config_entry: ConfigEntry) -> None:
+        self._client = client
+        self._config_entry = config_entry
+        self._attr_unique_id = f"blauberg_s21_{config_entry.unique_id}_timer_switch"
+
+    @property
+    def is_on(self) -> bool:
+        return self._client.device.is_timer
+
+    async def async_turn_on(self, **kwargs) -> None:
+        await self._client.set_timer_on()
+
+    async def async_turn_off(self, **kwargs) -> None:
+        await self._client.set_timer_off()
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        return DeviceInfo(
+            identifiers={(DOMAIN, self._config_entry.unique_id)},
+        )
